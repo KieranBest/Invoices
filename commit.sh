7de07c209b7d09cc55eccd88f827889412e5b8cd
Kieran Best <best.kieran.4595@gmail.com>
2024-03-27 15:08:54 +0000

    Generate docx

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git add Finances.csv
-timestamp() {
-    date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin main
--- script.py
+++
@@ -1,1 +0,0 @@
-print("You ran a script")
